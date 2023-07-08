206c36812cf1e45662458225eeaf5698eb626427
Manish Manwani <manishmanwani73@gmail.com>
2023-07-08 13:33:47 +0930

    feat(mobile filters): added mobile filters to individual category pages

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,7 @@ import Container from "@/components/ui/container";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
 import Filter from "./components/filter";
+import MobileFilters from "./components/mobile-filters";
 
 interface Props {
   params: {
@@ -35,7 +36,7 @@ export default async function CategoryPage({ params, searchParams }: Props) {
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            {/* Add mobile filters */}
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
