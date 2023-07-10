e633d4d7104daee416ae5405ec0e6cae41601a13
Manish Manwani <manishmanwani73@gmail.com>
2023-07-10 14:27:26 +0930

    refactor(home page): adding ts error to backlog

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -12,6 +12,7 @@ export default async function HomePage() {
   return (
     <Container>
       <div className="space-y-10 pb-10">
+        {/* @ts-ignore */}
         <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
