b61d2c13c57f36fef333ea420dd111047383a45e
Manish Manwani <manishmanwani73@gmail.com>
2023-07-10 12:33:57 +0930

    feat(cart page): added shopping cart page with cart items

---
+++ app/(routes)/cart/components/summary.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Summary() {
+  return <>Summary</>;
+}
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -3,6 +3,7 @@
 import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
 import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function NavbarActions() {
@@ -13,12 +14,15 @@ export default function NavbarActions() {
   }, []);
 
   const cart = useCart();
-
+  const router = useRouter();
   if (!isMounted) return null;
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button
+        className="flex items-center rounded-full bg-black px-4 py-2"
+        onClick={() => router.push("/cart")}
+      >
         <ShoppingBag size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white">
           {cart.items.length}
